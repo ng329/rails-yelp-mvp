6bb00a8261a5b4d2071b4e8aed795090c9ef5c65
Natasha Gill <natashamgill@hotmail.co.uk>
2022-08-24 15:03:41 +0100

    add reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :restaurants, only: %i[index create]
+  resources :restaurants, only: %i[index create show new] do
+    resources :reviews, only: %i[new create]
+  end
 end
