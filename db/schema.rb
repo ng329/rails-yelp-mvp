e605538a9be631a054c90b10e16b7ce38debaf45
Natasha Gill <natashamgill@hotmail.co.uk>
2022-08-24 11:37:12 +0100

    models review and restaurant set up with validations

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_101334) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
